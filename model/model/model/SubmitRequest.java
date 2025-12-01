71fe952a71caa4358d101d1580d4e7a3c358e33d
Anirudhvarma04 <anirudhv423@gmail.com>
2025-12-01 16:25:05 +0530

    Add SubmitRequest class for handling submissions

---
+++ model/model/model/SubmitRequest.java
@@ -0,0 +1,10 @@
+package com.example.bajaj.model;
+
+public class SubmitRequest {
+    private String finalQuery;
+
+    public SubmitRequest() {}
+    public SubmitRequest(String finalQuery) { this.finalQuery = finalQuery; }
+    public String getFinalQuery() { return finalQuery; }
+    public void setFinalQuery(String finalQuery) { this.finalQuery = finalQuery; }
+}
